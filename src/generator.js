48b022a5dfd60336e8b3e4aec7a122f87b42c548
Anna <ibraeva.ann@gmail.com>
2021-02-05 22:20:27 +0300

    add randomizer

---
+++ src/generator.js
@@ -0,0 +1,9 @@
+//генератор случайных чисел
+
+function getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max-min)) + min;
+}
+
+export { getRandomInt };
