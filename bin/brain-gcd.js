20b5da44f325316d91bc072a04f2abd46b99a063
Anna <ibraeva.ann@gmail.com>
2021-02-15 16:20:08 +0300

    add gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node;
+
+import brainGcd from '../src/games/gcd.js';
+
+brainGcd();
---
+++ src/games/gcd.js
@@ -0,0 +1,38 @@
+import readlineSync from 'readline-sync';
+import generator from '../generator.js';
+
+const findGCD = (a, b) => {
+    if (b === 0) {
+      return a;
+    }
+    return findGCD(b, a % b);
+  };
+
+const gameGcd = () => { 
+    console.log("Welcome to the Brain Games!");
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}`);
+    console.log('Find the greatest common divisor of given numbers.');
+
+    for (let i = 0; i < 3; i++) {
+        const num1 = generator(1, 100);
+        const num2 = generator(1, 100);
+        console.log(`Question:${num1}${num2}`);
+        const answer = readlineSync.question('Your answer:');
+
+        const rightAnswer = findGCD(num1, num);
+
+        if (answer === rightAnswer) {
+            console.log('Correct!');
+        }
+
+        else {
+            console.log(`${answer} is a wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${userName}!`);
+}
+
+export default gameGcd;
