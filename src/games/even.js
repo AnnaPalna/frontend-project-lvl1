fb9da4b76077a3349f8fd024b8ffb516c5776553
AnnaPalna <65242237+AnnaPalna@users.noreply.github.com>
2021-02-18 14:45:36 +0300

    upd even.js (lint)

--- src/games/even.js
+++ src/games/even.js
@@ -4,31 +4,30 @@ import generator from '../generator.js';
 const isEven = (number) => number % 2 === 0;
 
 const getEven = () => { 
-console.log("Welcome to the Brain Games!");
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}`);
-console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
-for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const num = generator(1, 100);
     console.log(`Question: ${num}`);
     const answer = readlineSync.question('Your answer:');
-    const rightAnswer = isEven(num) ? 'yes':'no';
+    const rightAnswer = isEven(num) ? 'yes' : 'no';
 
     const resultAnswer = answer.toLowerCase();
 
     if (resultAnswer === rightAnswer) {
-        console.log('Correct!');
+      console.log('Correct!');
     }
 
     else {
-        console.log(`Let's try again, ${userName}!`);
-        return;
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
 }
 
-console.log(`Congratulations, ${userName}!`);
-
+  console.log(`Congratulations, ${userName}!`);
 }
 
 export default getEven;
