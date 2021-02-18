8b0990e5ada0ea8e39d64339bcd98f224241ef08
AnnaPalna <65242237+AnnaPalna@users.noreply.github.com>
2021-02-18 14:40:24 +0300

    upd calculate(linter)

--- src/games/calculateGame.js
+++ src/games/calculateGame.js
@@ -2,27 +2,24 @@ import readlineSync from 'readline-sync';
 import generator from '../generator.js';
 
 //реализация функции сложения, вычитания, умножения
-
-
 const calculateGame = () => { 
-console.log("Welcome to the Brain Games!");
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}`);
-console.log('What is the result of the expression?');
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+  console.log('What is the result of the expression?');
 
 const makeOperation = (operator, x, y) => {
-    switch (operator) {
-        case '+':
-            return x + y;
-        case '-':
-            return x - y;
-        case '*':
-            return x * y;
+  switch (operator) {
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
         default:
-            return NaN;
+          return NaN;
     }
 }
-
 
 for (let i = 0; i < 3; i += 1) {
     const num1 = generator(1, 100);
@@ -35,17 +32,15 @@ for (let i = 0; i < 3; i += 1) {
     const rightAnswer = makeOperation(operator, num1, num2);
     
     if (Number(answer) === rightAnswer) {
-        console.log('Correct!');
+      console.log('Correct!');
     }
 
     else {
-        console.log(`${answer} is a wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
-        return;
+      console.log(`${answer} is a wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
+      return;
     }
 }
-
 console.log(`Congratulations, ${userName}!`);
-
 };
 
 export default calculateGame;
