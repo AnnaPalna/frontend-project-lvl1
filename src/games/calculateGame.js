0853c31db9fb9e13517d53ea458eb96dc1bfa767
AnnaPalna <65242237+AnnaPalna@users.noreply.github.com>
2021-02-18 16:53:30 +0300

    Update calculateGame.js

--- src/games/calculateGame.js
+++ src/games/calculateGame.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 import generator from '../generator.js';
 
-//реализация функции сложения, вычитания, умножения 
 const calculateGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
@@ -19,7 +18,7 @@ const calculateGame = () => {
       default:
         return NaN;
     }
-  }
+  };
 
   for (let i = 0; i < 3; i += 1) {
     const num1 = generator(1, 100);
@@ -28,16 +27,14 @@ const calculateGame = () => {
     const operator = operators[generator(0, operators.length - 1)];
     console.log(`Question: ${num1} ${operator} ${num2}`);
     const answer = readlineSync.question('Your answer:');
-    
     const rightAnswer = makeOperation(operator, num1, num2);
-    
     if (Number(answer) === rightAnswer) {
       console.log('Correct!');
     } else {
       console.log(`${answer} is a wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
       return;
     }
-}
+  }
   console.log(`Congratulations, ${userName}!`);
 };
 
