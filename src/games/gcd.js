a1e091cd9f1c55477a6c71d8dca3774c2d1e527b
AnnaPalna <65242237+AnnaPalna@users.noreply.github.com>
2021-02-18 15:15:21 +0300

    upd gcd - lint

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,37 +2,34 @@ import readlineSync from 'readline-sync';
 import generator from '../generator.js';
 
 const brainGcd = () => { 
-    console.log("Welcome to the Brain Games!");
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}`);
-    console.log('Find the greatest common divisor of given numbers.');
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+  console.log('Find the greatest common divisor of given numbers.');
 
-    const findGCD = (a, b) => {
+  const findGCD = (a, b) => {
     if (b === 0) {
       return a;
     }
     return findGCD(b, a % b);
     };
-    
-
     for (let i = 0; i < 3; i += 1) {
-        const num1 = generator(1, 100);
-        const num2 = generator(1, 100);
-        console.log(`Question: ${num1} ${num2}`);
-        const answer = readlineSync.question('Your answer:');
+      const num1 = generator(1, 100);
+      const num2 = generator(1, 100);
+      console.log(`Question: ${num1} ${num2}`);
+      const answer = readlineSync.question('Your answer:');
 
-        const rightAnswer = findGCD(num1, num2);
+      const rightAnswer = findGCD(num1, num2);
 
         if (Number(answer) === rightAnswer) {
-            console.log('Correct!');
+          console.log('Correct!');
         }
 
         else {
-            console.log(`${answer} is a wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
-            return;
+          console.log(`${answer} is a wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${userName}!`);
+          return;
         }
     }
-
     console.log(`Congratulations, ${userName}!`);
 }
 
